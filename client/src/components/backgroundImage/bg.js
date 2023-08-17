3c6cf881367bd56bce15fa30623540b12969a32b
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-08-17 23:58:09 +0530

    changing BG

---
+++ client/src/components/backgroundImage/bg.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./bg.css"; // Import the background CSS file
+
+const images = [
+  "https://images6.alphacoders.com/132/1322318.jpeg",
+  "https://4kwallpapers.com/images/wallpapers/beautiful-girl-woman-monochrome-dark-background-portrait-3840x2160-2965.jpeg", // Add more image URLs here
+  "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=2000",
+  // ... add more image URLs
+];
+
+const BGComponent = ({ children }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000); // Change image every 5 seconds
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div
+      className="background"
+      style={{
+        backgroundImage: `url(${images[currentImageIndex]})`,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default BGComponent;
