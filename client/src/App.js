56daf7470185be4c724960f41405bcf1edbeea70
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2023-08-18 22:43:07 +0530

    Changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,37 @@ import Search from "./components/search/search";
 import NavigationBar from "./components/navBar/navigationBar";
 import BackImage from "./components/backdrop/backimage";
 import Container from "@mui/material/Container";
+import DiscoverCard from "./components/discoverCard/discover";
+import EnterDetails from "./components/NumberofPassengers/enterDetails";
+import PassengerDetailsForm from "./components/passengerDetails/PassengerDetailsForm";
+import Booking from "./pages/booking/booking";
+import NumberSpinner from "./components/numberSpinner/numSpinner";
+import BackgroundComponent from "./components/backgroundImage/background";
+import SpacecraftSeatView from "./components/seatView/seat";
+import Checkincard from "./components/cheksin_card/checkincard";
+import Home from "./pages/home/home";
+import BGComponent from "./components/backgroundImage/bg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BackImage />
+        {/* <BackImage />
         <NavigationBar />
         <Container>
+           <DiscoverCard />
           <Search />
           <Footer />
-        </Container>
+        </Container> */}
+
+        {/* <SpacecraftSeatView /> */}
+        <Booking />
+        {/* <Booking /> */}
+        <BGComponent>
+          <Footer />
+        </BGComponent>
+        <Search />
+        <Card date={"ghcjc"} />
       </header>
     </div>
   );
