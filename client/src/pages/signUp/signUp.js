48eeb5b99ec78a2610a8064f66a8574b20bef432
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2023-08-19 14:12:23 +0530

    SignUp Form

---
+++ client/src/pages/signUp/signUp.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "./signUp.css";
+import PinkButton from "../../components/button/button";
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Basic form validation
+    if (
+      !formData.fullName ||
+      !formData.email ||
+      !formData.password ||
+      !formData.confirmPassword
+    ) {
+      setError("All fields are required.");
+      return;
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+
+    // Here you can implement your actual authentication logic
+    // For simplicity, I'll just log the data to the console
+    console.log("Form data:", formData);
+
+    // Reset form data and error message
+    setFormData({
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+    setError("");
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Full Name"
+          value={formData.fullName}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+        {error && <p className="error">{error}</p>}
+        <PinkButton type="submit" text={"Sign Up"} />
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
