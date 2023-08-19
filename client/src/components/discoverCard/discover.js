475c418dd982cd072b958236c7567ec7de164a2c
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2023-08-19 22:55:40 +0530

    updated discover.js

--- client/src/components/discoverCard/discover.js
+++ client/src/components/discoverCard/discover.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./discover.css"; // Import the corresponding CSS file
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
-const DiscoverCard = ({ planetName }) => {
+const DiscoverCard = ({
+  planetName,
+  subtext,
+  subsubtext,
+  MyBackgroundImage,
+}) => {
   return (
     <Link to={`/planet/${planetName}`}>
       <div className="dis">
-        <div className="img-container-dis"></div>
+        <div
+          className="img-container-dis"
+          style={{ backgroundImage: `url(${MyBackgroundImage})` }}
+        ></div>
         <div className="fading-image-dis">
           <div className="text">{planetName}</div>
+          <div className="sub-text">{subtext}</div>
+          <div className="sub-sub-text">{subsubtext}</div>
         </div>
       </div>
     </Link>
