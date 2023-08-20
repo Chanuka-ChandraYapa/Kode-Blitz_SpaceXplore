f3ea6c69beeabc71aeaa59b44c4a5695b1a653ad
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-08-20 09:27:34 +0530

    authorized redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,59 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Card from "./components/class card/card";
-// import ProfilePageMobile from "./pages/profile";
-import NewCard from "./components/Aluthcardeka/new_card";
-
-import Footer from "./components/footer/footer";
-
-import BackgroundComponent from "./components/backgroundImage/background";
-import Calendar from "./components/calendar/calendar";
-
-import CardSlider from "./cardSlider";
-
-import ImageSlider from "./components/slidingCard/Slideshow";
-import BGComponent from "./components/backgroundImage/bg";
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import React, { useState, createContext, useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import NavigationBar from "./components/navBar/navigationBar";
+import Home from "./pages/home/home";
+import SignUpForm from "./pages/signUp/signUp";
+import SigninForm from "./pages/signIn/signIn";
 import ProfilePageMobile from "./pages/profile/profile";
 import Planet from "./pages/planet/planet";
 import Booking from "./pages/booking/booking";
 
+// Create an AuthContext to manage authentication state
+const AuthContext = createContext();
+
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true); // Change as needed
+
   return (
-    <Router>
-      <div>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <Router>
+        {/* <NavigationBar /> */}
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/profile" element={<ProfilePageMobile />} />
-          <Route path="/planet" element={<Planet />} />
-          <Route path="/booking" element={<Booking />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/Signin" element={<SigninForm />} />
+
+          <Route exact path="/" element={<PrivateRoute Component={Home} />} />
+          <Route
+            exact
+            path="/profile"
+            element={<PrivateRoute Component={ProfilePageMobile} />}
+          />
+          <Route
+            exact
+            path="/planet"
+            element={<PrivateRoute Component={Planet} />}
+          />
+          <Route
+            exact
+            path="/booking"
+            element={<PrivateRoute Component={Booking} />}
+          />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
+// Custom PrivateRoute component
+function PrivateRoute({ Component }) {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return isAuthenticated ? <Component /> : <Navigate to="/Signin" />;
+}
+
 export default App;
-
-{/* <div>
-  <NewCard
-    imageUrl="https://wallpaperaccess.com/full/3136563.jpg"
-    title="Hey There"
-  />
-</div> */}
-
-// import logo from "./logo.svg";
-// import "./App.css";
-// import Card from "./components/class card/card";
-// import ProfilePageMobile from "./pages/profile";
-// import New_card from "./components/Aluthcardeka/new_card";
-// import Footer from "./components/footer/footer";
-// import Checkincard from "./components/cheksin_card/checkincard";
-// import BackgroundComponent from "./components/backgroundImage/background";
-// import PriceSummery from "./components/priceSummery/priceSummery";
-// import Calendar from "./components/calendar/calendar";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <New_card
-//           imageUrl="https://wallpaperaccess.com/full/3136563.jpg"
-//           title="Hey There"
-//         />
-//       </header>
-//       {/* <BackgroundComponent>
-//         <p>Hello</p>
-//         <Calendar date={"2023-08-16"} />
-
-//         <Footer />
-//       </BackgroundComponent> */}
-//     </div>
-//   );
-// }
-
-// export default App;
