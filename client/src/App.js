28cb41f7e165ada0da87ed48c26479ad1454e4c5
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-08-20 09:54:04 +0530

    Adding All Private Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,90 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Card from "./components/class card/card";
+
 // import ProfilePageMobile from "./pages/profile";
-import NewCard from "./components/Aluthcardeka/new_card";
+import { useState, useContext, createContext} from "react";
 
-import Footer from "./components/footer/footer";
-
-import BackgroundComponent from "./components/backgroundImage/background";
-import Calendar from "./components/calendar/calendar";
-
-import CardSlider from "./cardSlider";
-
-import ImageSlider from "./components/slidingCard/Slideshow";
-import BGComponent from "./components/backgroundImage/bg";
-
-
-import { BrowserRouter as Router, Routes, Route,Switch } from "react-router-dom";
-
+import NavigationBar from "./components/navBar/navigationBar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/home/home";
 import ProfilePageMobile from "./pages/profile/profile";
 import Planet from "./pages/planet/planet";
 import Booking from "./pages/booking/booking";
+import SignUp from "./pages/signUp/signUp";
+import SigninForm from "./pages/signIn/signIn";
+import Landing from "./pages/landing/landing";
+import Yet from "./pages/ToBeImplemented/yet";
+
+const AuthContext = createContext();
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(true); // Change as needed
 
   return (
-    <Router>
-      <div>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/Signin" element={<SigninForm />} />
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <Router>
+        <div>
+          <div className="App">
+            <div className="App-header">
+              <NavigationBar />
 
-          <Route exact path="/" element={<PrivateRoute Component={Home} />} />
-          <Route
-            exact
-            path="/profile"
-            element={<PrivateRoute Component={ProfilePageMobile} />}
-          />
-          <Route
-            exact
-            path="/planet"
-            element={<PrivateRoute Component={Planet} />}
-          />
-          <Route
-            exact
-            path="/booking"
-            element={<PrivateRoute Component={Booking} />}
-          />
-        </Routes>
-      </div>
-    </Router>
+              <Routes>
+                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/Signin" element={<SigninForm />} />
+                <Route path="/landing" element={<Landing />} />
+                <Route
+                  exact
+                  path="/"
+                  element={<PrivateRoute Component={Home} />}
+                />
+                <Route
+                  exact
+                  path="/profile"
+                  element={<PrivateRoute Component={ProfilePageMobile} />}
+                />
+                <Route
+                  exact
+                  path="/planet"
+                  element={<PrivateRoute Component={Planet} />}
+                />
+                <Route
+                  exact
+                  path="/booking"
+                  element={<PrivateRoute Component={Booking} />}
+                />
+                <Route
+                  exact
+                  path="/planet/:planetName"
+                  element={<PrivateRoute Component={Planet} />}
+                />
+                <Route
+                  exact
+                  path="/flights"
+                  element={<PrivateRoute Component={Yet} />}
+                />
+                <Route
+                  exact
+                  path="/team"
+                  element={<PrivateRoute Component={Yet} />}
+                />
+                <Route
+                  exact
+                  path="/about"
+                  element={<PrivateRoute Component={Yet} />}
+                />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
@@ -62,5 +96,3 @@ function PrivateRoute({ Component }) {
 }
 
 export default App;
-
-
